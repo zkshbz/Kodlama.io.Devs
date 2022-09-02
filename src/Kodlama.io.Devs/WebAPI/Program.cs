bcded4fbe1b9a017d5aa98d5bc236afa3f6f1956
zkshbz <zekisahbaz@outlook.com>
2022-09-02 23:09:09 +0300

    database created

--- src/Kodlama.io.Devs/WebAPI/Program.cs
+++ src/Kodlama.io.Devs/WebAPI/Program.cs
@@ -1,9 +1,12 @@
+using Core.CrossCuttingConcerns.Exceptions;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddPersistenceServices(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,6 +20,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+if (app.Environment.IsProduction())
+{
+    app.ConfigureCustomExceptionMiddleware();
+}
 
 app.UseAuthorization();
 
